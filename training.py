439d87776309aabec189736062e6a8d4e10f609b
abinbs <abinbinusam@gmail.com>
2020-10-07 21:17:50 +0530

    Model Structure

--- training.py
+++ training.py
@@ -1,48 +1,110 @@
 import keras
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Dense,Dropout,Activation,Flatten,BatchNormalization
+from keras.layers import Conv2D,MaxPooling2D
 import os
 
-num_classes= 5
-img_rows,img_cols= 48,48
-batch_size= 8
+num_classes = 5
+img_rows,img_cols = 48,48
+batch_size = 32
 
-train_data_dir= r'D:\Facial Recognition\images\train'  #since python takes forward slash r reverses the slash of the directory path
-validation_data_dir= r'D:\Facial Recognition\images\validation'
+train_data_dir = r'D:\Facial Recognition\images\train' #since python takes forward slash r reverses the slash of the directory path
+validation_data_dir = r'D:\Facial Recognition\images\validation'
 
-train_datagen= ImageDataGenerator(      #images are generated for training
-    rescale= 1./255, #reduce image size
-    rotation_range= 30,
-    shear_range= 0.3,
-    zoom_range= 0.3,
-    width_shift_range= 0.4,
-    height_shift_range= 0.4,   #create a new image by shifting accordingly
-    horizontal_flip= True,      #Mirror image
-    vertical_flip= True
-)
+train_datagen = ImageDataGenerator(		 #images are generated for training
+					rescale=1./255,		#reduce image size
+					rotation_range=30,
+					shear_range=0.3,
+					zoom_range=0.3,
+					width_shift_range=0.4,
+					height_shift_range=0.4,		#create a new image by shifting accordingly
+					horizontal_flip=True,		#Mirror image
+					fill_mode='nearest')
 
-validation_datagen= ImageDataGenerator(
-    rescale= 1./255
-)
+validation_datagen = ImageDataGenerator(rescale=1./255)
 
-train_generator= train_datagen.flow_from_directory(       #Training the data
-    train_data_dir,
-    color= 'grayscale',
-    target_size= (img_rows,img_cols),
-    batch_size= batch_size,
-    class_mode= 'categorical',
-    shuffle= True       #Shuffling all the images for better training, so it would memorize all the category images
-)
+train_generator = train_datagen.flow_from_directory(		#Training the data
+					train_data_dir,
+					color_mode='grayscale',
+					target_size=(img_rows,img_cols),
+					batch_size=batch_size,
+					class_mode='categorical',
+					shuffle=True)		#Shuffling all the images for better training, so it would memorize all the category images
 
-validation_generator= validation_datagen.flow_from_directory(       #Validating the data
-    validation_data_dir,
-    color= 'grayscale',
-    target_size= (img_rows,img_cols),
-    batch_size= batch_size,
-    class_mode= 'categorical',
-    shuffle= True  
-)
+validation_generator = validation_datagen.flow_from_directory(		#Validating the data
+							validation_data_dir,
+							color_mode='grayscale',
+							target_size=(img_rows,img_cols),
+							batch_size=batch_size,
+							class_mode='categorical',
+							shuffle=True)
 
-#TODO- cnn model
+
+model = Sequential()
+
+# Block-1
+
+model.add(Conv2D(32,(3,3),padding='same',kernel_initializer='he_normal',input_shape=(img_rows,img_cols,1)))	#creating the first box with input size 32
+model.add(Activation('elu'))	 #Threshold value for activating the prediction		
+model.add(BatchNormalization())		#increase training spped and performance
+model.add(Conv2D(32,(3,3),padding='same',kernel_initializer='he_normal',input_shape=(img_rows,img_cols,1)))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))	#finds the max element in the selected area
+model.add(Dropout(0.2))		#prevents overfittiing
+
+# Block-2 
+
+model.add(Conv2D(64,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(64,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+
+# Block-3
+
+model.add(Conv2D(128,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(128,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+
+# Block-4 
+
+model.add(Conv2D(256,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(256,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+
+# Block-5
+
+model.add(Flatten())		#compress data to single long vector
+model.add(Dense(64,kernel_initializer='he_normal'))		 #Fully connected layer
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+
+# Block-6
+
+model.add(Dense(64,kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+
+# Block-7
+
+model.add(Dense(num_classes,kernel_initializer='he_normal'))
+model.add(Activation('softmax'))		#softmax is used as we have 5 classes, sigmoid used if only 2 class
+
+print(model.summary())		#summary of our created model
