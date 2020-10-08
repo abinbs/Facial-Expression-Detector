9b6525fed6a4f6f2aa4a4ff46c84a67f96983f7c
abinbs <abinbinusam@gmail.com>
2020-10-08 18:39:47 +0530

    Pre-trained model

--- training.py
+++ training.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import keras
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
@@ -5,7 +6,7 @@ from keras.layers import Dense,Dropout,Activation,Flatten,BatchNormalization
 from keras.layers import Conv2D,MaxPooling2D
 import os
 
-num_classes = 5
+num_classes = 7
 img_rows,img_cols = 48,48
 batch_size = 32
 
@@ -108,3 +109,45 @@ model.add(Dense(num_classes,kernel_initializer='he_normal'))
 model.add(Activation('softmax'))		#softmax is used as we have 5 classes, sigmoid used if only 2 class
 
 print(model.summary())		#summary of our created model
+
+from keras.optimizers import RMSprop,SGD,Adam
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+
+checkpoint = ModelCheckpoint(r'C:\Users\USER\Desktop\FR\Emotion_little_vgg.h5',		#new pre-trained model
+                             monitor='val_loss',		#measuring performance inorder to end training
+                             mode='min',
+                             save_best_only=True,
+                             verbose=1)
+
+earlystop = EarlyStopping(monitor='val_loss',		#training stopped early if validation accuracy starts decreasing
+                          min_delta=0,
+                          patience=3,		#if 3 epochs are done without any improvement, training is stopped
+                          verbose=1,
+                          restore_best_weights=True
+                          )
+
+reduce_lr = ReduceLROnPlateau(monitor='val_loss',
+                              factor=0.2,
+                              patience=3,
+                              verbose=1,
+                              min_delta=0.0001)
+
+callbacks = [earlystop,checkpoint,reduce_lr]		#for interacting with the model
+
+model.compile(loss='categorical_crossentropy',
+              optimizer = Adam(lr=0.001),	#faster algorithm with individual learning rates
+              metrics=['accuracy'])
+
+nb_train_samples = 24176		#total no. of training images
+nb_validation_samples = 3006	#total no. of validation images
+epochs=25						#no. of times an image is trained
+
+history=model.fit_generator(
+                train_generator,
+                steps_per_epoch=nb_train_samples//batch_size,
+                epochs=epochs,
+                callbacks=callbacks,
+                validation_data=validation_generator,
+                validation_steps=nb_validation_samples//batch_size)
+
+
